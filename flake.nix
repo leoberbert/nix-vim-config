02e0c2466270c56fc7a98f8adec684439ff24396
leoberbert <leo4berbert@gmail.com>
2025-04-03 18:26:50 -0300

    Correção do sha-256

--- flake.nix
+++ flake.nix
@@ -6,33 +6,60 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.default =
       let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+
         codedark = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/tomasiser/vim-code-dark/master/colors/codedark.vim";
-        #sha256 = pkgs.lib.fakeSha256;
-        sha256 = "sha256-4/BhTIj6tAkJ4d6fIQfQ5bEmPZ1LegEak0SR4TR0sZU=";
+          url = "https://raw.githubusercontent.com/tomasiser/vim-code-dark/master/colors/codedark.vim";
+          sha256 = "sha256-4/BhTIj6tAkJ4d6fIQfQ5bEmPZ1LegEak0SR4TR0sZU=";
         };
 
-      in
-        pkgs.stdenv.mkDerivation {
-          pname = "nix-vim-config";
-          version = "1.0";
-          src = ./.;
+        myVim = pkgs.vim_configurable.customize {
+          name = "vim";
+          vimrcConfig.customRC = ''
+            syntax on
+            filetype plugin indent on
+            set nocompatible
+            colorscheme codedark
 
-          nativeBuildInputs = [ pkgs.vim ];
-
-          installPhase = ''
-            mkdir -p $out/etc
-            cp $src/vimrc $out/etc/vimrc
-
-            mkdir -p $out/share/vim/vimfiles/colors
-            cp ${codedark} $out/share/vim/vimfiles/colors/codedark.vim
+            set background=dark
+            set laststatus=2
+            set cursorline
+            set hlsearch
           '';
-
-          meta = {
-            description = "Custom Vim configuration with codedark colorscheme";
-            license = pkgs.lib.licenses.gpl3;
-          };
         };
+      in pkgs.stdenv.mkDerivation {
+        pname = "nix-vim-config";
+        version = "1.0";
+        src = ./.;  # 👈 ESSENCIAL
+
+        buildInputs = [ myVim ];
+
+	installPhase = ''
+	  mkdir -p $out/bin
+	  ln -s ${myVim}/bin/vim $out/bin/vim
+	
+	  mkdir -p $out/share/vim/vimfiles/colors
+	  cp ${codedark} $out/share/vim/vimfiles/colors/codedark.vim
+	
+	  mkdir -p $out/etc
+	  cat > $out/etc/vimrc <<EOF
+	syntax on
+	filetype plugin indent on
+	set nocompatible
+	colorscheme codedark
+	
+	set background=dark
+	set laststatus=2
+	set cursorline
+	set hlsearch
+	EOF
+	'';
+        meta = {
+          description = "Custom Vim with codedark colorscheme";
+          license = pkgs.lib.licenses.gpl3;
+        };
+      };
   };
 }
+

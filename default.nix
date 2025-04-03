da6a6c167978d41abf5b2ea05c13d43de2835d58
leoberbert <leo4berbert@gmail.com>
2025-04-03 00:17:16 -0300

    fix: Config vim Nix

--- default.nix
+++ default.nix
@@ -1,20 +1,28 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  codedark = pkgs.fetchFromGitHub {
+    owner = "tomasiser";
+    repo = "vim-code-dark";
+    rev = "4624dc2";  # Commit existente!
+    sha256 = "sha256-fCPetq4Wr/kuaT0Pcd6NSEAUBkkjbCE1PhTt0KLj9oI="; # fake sha
+  };
+in
+
 pkgs.stdenv.mkDerivation {
   pname = "nix-vim-config";
   version = "1.0";
 
   src = ./.;
 
-  nativeBuildInputs = [ pkgs.vim pkgs.git pkgs.curl ];
+  nativeBuildInputs = [ pkgs.vim pkgs.git ];
 
   installPhase = ''
     mkdir -p $out/etc
     cp $src/vimrc $out/etc/vimrc
 
     mkdir -p $out/share/vim/vimfiles/colors
-    curl -L https://raw.githubusercontent.com/tomasiser/vim-code-dark/master/colors/codedark.vim \
-      -o $out/share/vim/vimfiles/colors/codedark.vim
+    cp ${codedark}/colors/codedark.vim $out/share/vim/vimfiles/colors/codedark.vim
   '';
 
   meta = {

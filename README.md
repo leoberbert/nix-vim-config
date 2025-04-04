03956f448a5e887049824b73384217f98148a091
leoberbert <leo4berbert@gmail.com>
2025-04-04 14:31:55 -0300

    Correção da configuração do vim

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# nix-vim-config
+# 🎨 nix-vim-config
 
-Vim Configuration
+Este projeto fornece uma configuração personalizada do **Vim** empacotada como um **flake Nix**, incluindo:
+
+- Tema de cores: [`codedark`](https://github.com/tomasiser/vim-code-dark)
+- Realce de sintaxe
+- Indentação personalizada por tipo de arquivo (HTML, YAML, Python)
+- Atalhos visuais e configurações de usabilidade
+
+> Ideal para quem quer um Vim pronto para uso com boa aparência e configuração inteligente.
+
--- flake.nix
+++ flake.nix
@@ -9,57 +9,84 @@
         system = "x86_64-linux";
         pkgs = import nixpkgs { inherit system; };
 
+        # Baixa o tema codedark
         codedark = pkgs.fetchurl {
           url = "https://raw.githubusercontent.com/tomasiser/vim-code-dark/master/colors/codedark.vim";
           sha256 = "sha256-4/BhTIj6tAkJ4d6fIQfQ5bEmPZ1LegEak0SR4TR0sZU=";
         };
 
+        # Tema como plugin extra
+        codedarkPlugin = pkgs.vimUtils.buildVimPlugin {
+          name = "codedark";
+          src = pkgs.runCommand "codedark-src" { } ''
+            mkdir -p $out/colors
+            cp ${codedark} $out/colors/codedark.vim
+          '';
+        };
+
+        # Vim com plugin e configurações
         myVim = pkgs.vim_configurable.customize {
           name = "vim";
           vimrcConfig.customRC = ''
-            syntax on
-            filetype plugin indent on
-            set nocompatible
-            colorscheme codedark
-
-            set background=dark
-            set laststatus=2
-            set cursorline
-            set hlsearch
-          '';
-        };
-      in pkgs.stdenv.mkDerivation {
-        pname = "nix-vim-config";
-        version = "1.0";
-        src = ./.;  # 👈 ESSENCIAL
-
-        buildInputs = [ myVim ];
-
-	installPhase = ''
-	  mkdir -p $out/bin
-	  ln -s ${myVim}/bin/vim $out/bin/vim
-	
-	  mkdir -p $out/share/vim/vimfiles/colors
-	  cp ${codedark} $out/share/vim/vimfiles/colors/codedark.vim
-	
-	  mkdir -p $out/etc
-	  cat > $out/etc/vimrc <<EOF
-	syntax on
+          	syntax on
 	filetype plugin indent on
-	set nocompatible
+
+	" Search and encoding settings
+
+	set noincsearch                        " Disables incremental search; matches are only highlighted after pressing Enter
+	set ignorecase                         " Ignores case in search patterns for easier matching
+	set encoding=utf8                      " Sets file encoding to UTF-8 for better compatibility
+	set laststatus=2                       " Always displays the status line at the bottom of the screen
+	set hlsearch                           " Highlights all matches for search results to improve visibility
+	set showmatch                          " Briefly highlights matching parentheses, brackets, or braces
+	set cursorline                         " Highlights the entire line where the cursor is positioned for better focus
+	set nocompatible                       " Ensures Vim is not in Vi mode
+
+	"Color Scheme
 	colorscheme codedark
-	
-	set background=dark
-	set laststatus=2
-	set cursorline
-	set hlsearch
-	EOF
-	'';
-        meta = {
-          description = "Custom Vim with codedark colorscheme";
-          license = pkgs.lib.licenses.gpl3;
+
+	" Enables mouse support if available
+	if has("mouse")
+	    set mouse=v
+	endif
+
+
+	" Configures indentation settings specifically for HTML files
+	function HtmlConfig()
+	        set tabstop=2 softtabstop=2 expandtab shiftwidth=2
+	        " Sets tab, soft tab, and indentation width to 2 spaces for HTML files
+	endfunction
+
+	" Configures indentation settings specifically for Python files
+	function PythonConfig()
+	        set tabstop=4 softtabstop=4 expandtab shiftwidth=4
+	        " Sets tab, soft tab, and indentation width to 4 spaces for Python files
+	endfunction
+
+	" Configures indentation settings specifically for YAML files
+	function YamlConfig()
+	        set tabstop=2 softtabstop=2 expandtab shiftwidth=2 indentkeys-=0# indentkeys-=<:>
+	        " Sets tab, soft tab, and indentation width to 2 spaces for YAML files,
+	        " with custom indent keys
+	endfunction
+
+	" Autocommands for automatic behavior
+	autocmd CursorHoldI * stopinsert       " Exits Insert mode if cursor is idle for updatetime duration
+	autocmd FileType html call HtmlConfig()   " Applies HtmlConfig() for HTML files
+	autocmd FileType python call PythonConfig() " Applies PythonConfig() for Python files
+	autocmd FileType yaml,yml call YamlConfig() " Applies YamlConfig() for YAML files
+
+	" Status line configuration to show paste mode status, file information, working directory,
+	" and cursor position details
+	set statusline=\ File:\ %F%m%r%h\ %w\ \ Working\ Directory:\ %r%{getcwd()}%h\ -\ Line:\ %l\ -\ Column:\ %c
+	" Prevents defaults.vim from overwriting these settings
+	let g:skip_defaults_vim = 1
+
+          '';
+          vimrcConfig.packages.myPlugins = with pkgs.vimPlugins; {
+            start = [ codedarkPlugin ];
+          };
         };
-      };
+      in myVim;
   };
 }
-
